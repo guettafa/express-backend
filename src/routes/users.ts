e9dbae857e258ceb47b12b13c57c230074abc8fa
guettafa <irontitou5@gmail.com>
2024-10-07 19:08:29 -0400

    Added auth middleware in users.ts

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,7 +1,8 @@
-import express, {Request,Response} from "express";
+import express, {NextFunction, Request, Response} from "express";
 
 import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs"
+import { decode } from "punycode";
 
 const router = express.Router();
 
@@ -34,5 +35,24 @@ router.post("/register", async (req: Request, res: Response) => {
         pwd: hashedPassword
     });
 })
+
+// Auth middleware
+const checkAccess = (req: any, res: Response, next: NextFunction) => {
+    const token: any = req.headers['authorization'];
+
+    if (!token) {
+        res.status(403).json({ message: "Token is missing" });
+    }
+    
+    // Separate Bearer and token
+    jwt.verify(token?.split(' ')[1], SECRET_KEY, (error: any, decoded: any) => {
+        if (error) {
+            res.status(403).json("Invalid Token")
+        }
+        // Retrieve token information
+        req.user = decoded
+        next();
+    })
+}
 
 export default router;
