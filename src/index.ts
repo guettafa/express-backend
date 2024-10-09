46fec40e4dc387940b71e7e44fcc3196427d55d8
guettafa <irontitou5@gmail.com>
2024-10-08 20:38:18 -0400

    Changed import file name in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,24 @@
 import express, { Request, Response } from "express";
+import productsRoutes from "./routes/products.route";
+import usersRoutes from "./routes/users.route";
 
-import productsRoutes from "./routes/products";
-import usersRoutes from "./routes/users";
+import { config } from "./config/config";
 
 const app = express();
-const PORT = 3000;
+const PORT = config.port;
 
 // Parse Responses to JSON 
 app.use(express.json()),
 
+// Root
 app.get("/", (req: Request, res: Response) => {
     res.send("Hello World");
 });
 
-// /products
+// Products
 app.use("/products", productsRoutes)
+
+// Users
 app.use("/users", usersRoutes)
 
 app.listen(PORT, () => {
