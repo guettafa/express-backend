b156c6fb105006870368f5785f069a102acaa450
guettafa <irontitou5@gmail.com>
2024-10-12 16:46:32 -0400

    Added users.json file

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,8 +1,12 @@
+import { User } from "../models/user";
+import { readJSON, saveJSON } from "../utils/jsonHelper";
 
+const PATH_JSON_USERS = "./src/data/users.json";
 
+let users: User[] = readJSON<User>(PATH_JSON_USERS);
 
-export const getUsers = () => {
-    
+export const getUsers = (): User[] => {
+   return users;
 }
 
 export const getUser = (id: number) => {
